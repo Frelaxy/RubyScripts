b2df5f35f3b6bf921c6b6e3cdc5a7be172f7c01f
Frelaxy <kiryl.masliukou@activeplatform.com>
2023-09-09 01:40:47 +0300

    refactoring

--- Scripts for tickets/SAP-19616.rb
+++
@@ -1,4 +0,0 @@
-product_cat_ids = ProductCategory.where(key: "gsuite").ids
-products = Product.where(product_category: product_cat_ids)
-plans = Plan.where(product_id: products.ids)
-PlanResource.where(plan_id: plans.ids, name: "Users")
--- Scripts for tickets/disable_autorenew_SAP-18572.rb
+++
@@ -1,6 +0,0 @@
-resellers_ids = %w[419 349 337]
-plan_categories_ids = PlanCategory.where(reseller_id: resellers_ids).where("key = 'microsoft_csp_annually' or key ='microsoft_csp_monthly'").ids
-plans_ids = Plan.where(plan_category_id: plan_categories_ids).ids
-subscriptions = Subscription.where(plan_id: plans_ids).where.not(status: :deleted).where("expiration_date > ?", Date.new(2023,2,12))
-subscriptions.update(auto_renewal: false)
-subscriptions.update(auto_renewal_disabled: true)
